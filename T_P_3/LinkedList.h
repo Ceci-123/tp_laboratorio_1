b3480cf2b796a1ea4197c2eb6d34eec07a2dd42a
Ceci <78119165+Ceci-123@users.noreply.github.com>
2021-06-18 14:55:43 -0300

    linked list.h

--- T_P_3/LinkedList.h
+++ T_P_3/LinkedList.h
@@ -1,14 +1,17 @@
 /*
     utest example : Unit test examples.
     Copyright (C) <2018>  <Mauricio Davila>
+
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
+
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
+
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
@@ -28,8 +31,9 @@ struct LinkedList
 }typedef LinkedList;
 #endif
 
-
+//Publicas
 LinkedList* ll_newLinkedList(void);
+
 int ll_len(LinkedList* this);
 Node* test_getNode(LinkedList* this, int nodeIndex);
 int test_addNode(LinkedList* this, int nodeIndex,void* pElement);
